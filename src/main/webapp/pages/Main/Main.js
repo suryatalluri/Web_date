368085776eaf1dd8cf2263b8c1b8401aa6b7c012
surya talluri <surya.talluri@offline.com>
2017-05-11 10:14:08 +0000

    Version1 New files added - [calendar,live_widgets,Cities,securityService,hrdb,data] Files modified - [Main,leftnav,webapp,profiles,.wmproject.properties,pom.xml,project-services.xml,pages-config.json]

--- src/main/webapp/pages/Main/Main.js
+++ src/main/webapp/pages/Main/Main.js
@@ -15,3 +15,10 @@ Application.$controller("MainPageController", ["$scope", function ($scope) {
     };
 
 }]);
+
+Application.$controller("liveform1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
--- src/main/webapp/wmProperties.js
+++ src/main/webapp/wmProperties.js
@@ -1,13 +1,13 @@
 var _WM_APP_PROPERTIES = {
   "activeTheme" : "material",
-  "dateFormat" : "",
+  "dateFormat" : "MM/dd/yyyy",
   "defaultLanguage" : "en",
   "displayName" : "Web_date",
   "homePage" : "Main",
   "name" : "Web_date",
   "platformType" : "WEB",
   "supportedLanguages" : "en",
-  "timeFormat" : "",
+  "timeFormat" : "HH:mm:ss",
   "type" : "APPLICATION",
   "version" : "1.0"
 };
